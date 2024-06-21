7438b634aa6682fd66cfeef510b038b53815a2b2
adsjinf <jinfrj@hotmail.com>
2024-06-21 13:42:40 -0300

    Inicializando java + git + github

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Primeiro projeto integrando Java + Git + GitHub");
+    }
+}
