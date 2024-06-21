f6501ebcf85d3aa35c59795af62c236dffc7e3b1
adsjinf <jinfrj@hotmail.com>
2024-06-21 13:50:46 -0300

    Primeiro comentario do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+            /* primeiro push para cincronizar um git com github */
         System.out.println("Hello world!");
         System.out.println("Primeiro projeto integrando Java + Git + GitHub");
     }
